d07978b81132ff98477dba189f164e16d978d09b
raymond <taraymondl1@gmail.com>
2024-07-05 17:50:41 -0700

    Set up basic layout and functions
    Created a basic html layout for all the digits and operators using
    buttons. Created simple calculation, display, and clearDisplay functions
    in JS file. Linked digits and clear button to JS functions.

---
+++ calculator.js
@@ -0,0 +1,71 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(a, b, operation) {
+    switch (operation) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+    }
+}
+
+let firstVal;
+let secondVal;
+let operator;
+let displayVal;
+const display = document.querySelector(".display");
+/*
+const oneBtn = document.querySelector(".one");
+const twoBtn = document.querySelector(".two");
+const threeBtn = document.querySelector(".three");
+const fourBtn = document.querySelector(".four");
+const fiveBtn = document.querySelector(".five");
+const sixBtn = document.querySelector(".six");
+const sevenBtn = document.querySelector(".seven");
+const eightBtn = document.querySelector(".eigth");
+const nineBtn = document.querySelector(".nine");
+*/
+
+function updateDisplay() {
+    display.textContent = displayVal;
+}
+
+function clearDisplay() {
+    displayVal = 0;
+    firstVal = [];
+    secondVal = [];
+    operator = null;
+    updateDisplay();
+}
+
+function clickDigit(val) {
+    if (!operator) {
+        firstVal.push(val);
+        displayVal = firstVal.join('');
+    }
+    else {
+        secondVal.push(val);
+        displayVal = secondVal.join('');
+    }
+    updateDisplay()
+}
+
+clearDisplay();
